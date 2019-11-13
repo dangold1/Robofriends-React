2775fee217265843fcc3127cf6029a36606c6b6e
dangold1 <dangold308@gmail.com>
2019-11-13 13:52:32 +0200

    Updates

---
+++ precache-manifest.04e59558431dd346456d89893eeaf082.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30ebc188f4679175a87bd316ef87083",
+    "url": "/Robofriends-React/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/Robofriends-React/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "42f8252edf36027aa22a",
+    "url": "/Robofriends-React/static/css/main.9ca455a1.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/Robofriends-React/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "42f8252edf36027aa22a",
+    "url": "/Robofriends-React/static/js/main.7a9d4636.chunk.js"
+  },
+  {
+    "revision": "777d1897829abbc41676",
+    "url": "/Robofriends-React/static/js/runtime-main.3dc41792.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends-React/static/media/SEGA.12a3db8b.woff"
+  }
+]);
